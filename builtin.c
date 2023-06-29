ee5e973fe075f9e5bc7f8fd9561c70789a19508b
Nyashalo <lorrainegondo@gmail.com>
2023-06-29 05:33:32 -0700

    simple shell first part

--- builtin.c
+++ builtin.c
@@ -4,7 +4,8 @@
  * _myexit - exits the shell
  * @info: Structure containing potential arguments. Used to maintain
  * constant function prototype.
- * Return: 0 if info.argv[0] != "exit"
+ * Return: exits with a given exit status
+ * (0) if info.argv[0] != "exit"
  */
 int _myexit(info_t *info)
 {
